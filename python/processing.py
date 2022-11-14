741fb4448dd50baf9f10331a7602eb54b18e75f7
Sergej <35032543+sjkeller@users.noreply.github.com>
2022-11-14 19:41:30 +0100

    fixed gwn addition

--- python/processing.py
+++ python/processing.py
@@ -9,7 +9,7 @@ from sequences import gold_seq
 DPIEXPORT = 400
 MATSAVE = '/Users/sk/Library/CloudStorage/OneDrive-Persönlich/Studium/TUHH/3. Semester Master/Forschungsprojekt/uw-watermark-main/Watermark/input/signals/tSigTB_'
 WAVLOAD = '/Users/sk/Library/CloudStorage/OneDrive-Persönlich/Studium/TUHH/3. Semester Master/Forschungsprojekt/uw-watermark-main/Watermark/output/'
-CHANNEL = 'CASTLE3'
+CHANNEL = 'PLANE2'
 
 file_index = 0
 load_index = 0
@@ -23,26 +23,24 @@ rolloff = 1/8       # FIR cosine filter coefficent
 fc = 62.5e3         # carrier freqency
 sysOrd = 5          # order of butterworth filter
 
-targetSNR = 0.1        # targeted Signal Noise Ratio for addtive GWN generator
+#targetSNR = 0.1        # targeted Signal Noise Ratio for addtive GWN generator
 watermarkDelay = 12e-3  # delay of watermark simulation time of flight
 
 ### gold sequence generation
 deg = 10
 codeLen = 2 ** deg + 1
 
-""" def testPlotting(sig, time, num):
+def testPlotting(sig, time, num):
     fig = make_subplots(rows=2, cols=1)
     print(len(sig))
-    fig.add_trace(go.Scatter(y=sig, x=time), row=1, col=1)
+    fig.add_trace(go.Scatter(y=np.real(sig), x=time), row=1, col=1)
     fSigT, freqT = _get_fftfunc(sig, fs)
-    fig.add_trace(go.Scatter(y=freqT, x=np.abs(fSigT)), row=2, col=1)
+    fig.add_trace(go.Scatter(y=freqT, x=fSigT), row=2, col=1)
     mytitle = "Test " + str(num)
     fig.update_layout(title=mytitle)
-    fig.show() """
+    fig.show()
 
 def sig_noise_ratio(sig: np.ndarray, noise: np.ndarray):
-    #https://de.mathworks.com/help/signal/ref/mag2db.html
-    #https://de.mathworks.com/help/signal/ref/rssq.html
     snr = np.std(sig) / np.std(noise)
     return snr
 
@@ -88,7 +86,7 @@ def gen_tb_signal(time: np.ndarray, tSigBB: np.ndarray, saveMat: bool = True):
     global file_index
 
     ### shift spectrum to transmission band
-    tSigTB = np.real(tSigBB * np.exp(2.j*np.pi*fc*time))
+    tSigTB = tSigBB * np.exp(2.j*np.pi*fc*time)
 
     ### save signal as matlab object for watermark
     matobj = {'fs_x': fs, 'nBits': 0, 'x': tSigTB}
@@ -98,7 +96,7 @@ def gen_tb_signal(time: np.ndarray, tSigBB: np.ndarray, saveMat: bool = True):
 
     return (time, tSigTB)
 
-def gen_bb_signal(time: np.ndarray, tSigTB: np.ndarray):
+def get_tb_signal(sigLen: int):
     """ Shifts transfer band signal back to baseband
 
     Args
@@ -115,28 +113,20 @@ def gen_bb_signal(time: np.ndarray, tSigTB: np.ndarray):
 
     global load_index
 
-    filepath = CHANNEL + "/sigTB_" + "d" + str(deg) + "_" + str(load_index) + "/" + CHANNEL + "_001.wav"
+    filepath = CHANNEL + "/tSigTB_" + "d" + str(deg) + "_" + str(load_index) + "/" + CHANNEL + "_001.wav"
 
-    sigLen = len(tSigTB)
     _, tSigTBr = wavfile.read(WAVLOAD + filepath)
     
-    waveLen = loadmat(WAVLOAD + CHANNEL + "/sigTB_" + "d" + str(deg) + "_" + str(load_index) + "/bookkeeping.mat")
+    waveLen = loadmat(WAVLOAD + CHANNEL + "/tSigTB_" + "d" + str(deg) + "_" + str(load_index) + "/bookkeeping.mat")
     #waveLen = waveLen['bk'][0][0][2][0][1]
-    tSigTBr = tSigTBr[(watermarkDelay * fs):(sigLen + watermarkDelay * fs)]
+    lstWaveInd = waveLen['bk'][0][0][2]
+    sample_pick = np.random.randint(0, len(lstWaveInd))
+    print("index", lstWaveInd[sample_pick])
+    tSigTBr = tSigTBr[(lstWaveInd[sample_pick][0] + int(watermarkDelay * fs)):(lstWaveInd[sample_pick][0] + int(sigLen + watermarkDelay * fs))]
     load_index += 1
 
-    ### add white noise to not delayed part
-    gwn = gen_gwn_snr(tSigTBr, targetSNR)
-    tSigTBr += gwn
 
-    #testPlotting(np.real(tSigTB), time, 2)
-    print("SNR_dB: ", 10 * np.log10(sig_noise_ratio(tSigTB - gwn, gwn)), "dB")
-    print("SNR", sig_noise_ratio(tSigTB - gwn, gwn))
-
-    ### reshift spectrum to baseband
-    tSigBBr = tSigTBr * np.exp(2.j*np.pi*-fc*time)
-
-    return (time, tSigBBr)
+    return tSigTBr
 
 def delay_sum(signals: list[np.ndarray], delays: list[float], fs: float):
     """Creates a delayed sum of signals
@@ -162,7 +152,7 @@ def delay_sum(signals: list[np.ndarray], delays: list[float], fs: float):
 
     singalSumLen = int(np.floor(np.max(delays) * fs) + len(signals[0]) + 1)
 
-    signalSum = np.zeros(singalSumLen)
+    signalSum = np.zeros(singalSumLen, dtype = 'complex_')
     timeSum = np.linspace(0, Tsym * codeLen, singalSumLen)
     
     for s, d in zip(signals, delays):
@@ -238,11 +228,6 @@ def ca_cfar(x: np.ndarray, trBinSize: int, guBinSize: int, faRate: float, sort: 
         bin = x[i-binSize:i+binSize+1]
         guard = x[i-guBinSize:i+guBinSize+1]
 
-        #if sort:
-        #    bin = np.sort(bin)
-        #    guard = np.sort(bin)[(len(bin) - guBinSize):(len(bin) + guBinSize)]
-        # ignore guard cells for sorte averaging
-
         binSum = np.sum(bin)
         guBinSum = np.sum(guard)
 
@@ -284,10 +269,11 @@ def gen_gwn_snr(signal: np.ndarray, snr: float):
 
     return gwn
 
-def simulation(tDelays: list[float], numOfAnchors: int, addGWN = False, startSeed: int = 42, showAll: bool = False):
+def simulation(tDelays: list[float], numOfAnchors: int, addGWN = False, startSeed: int = 42, showAll: bool = False, targetSNR: float = 1.0, useSim: bool = True):
 
     figure = make_subplots(rows=numOfAnchors, cols=1)
-    tSendSig = []
+    lstAnchors = []
+    lstSignals = []
     
     for i in range(startSeed, startSeed + numOfAnchors):
 
@@ -299,31 +285,50 @@ def simulation(tDelays: list[float], numOfAnchors: int, addGWN = False, startSee
         filter = flt.rcosfilter(1024, rolloff, Tsym, fs)[1]
         tSigBB = sig.resample_poly(bCode, SpS, 1, window=filter)
 
+        ### use baseband signal for correlation later as anchor
+        lstAnchors.append(tSigBB)
+
         ### create fitting time axis
         time = np.linspace(0, Tsym * codeLen, len(tSigBB))
-
+        
         ### shift spectrum to transmission band and save it for simulation
-        time, tSigTB = gen_tb_signal(time, tSigBB)
+        time, tSigTB = gen_tb_signal(time, tSigBB, False)
+        
+        ### use simulation signal or just passthrough the generated transfer-band signal
+        if useSim:
+            tSigTBr = get_tb_signal(len(tSigTB))
+        else:
+            tSigTBr = np.real(tSigTB)
 
-        ### shift back to baseband loaded matlab wavefile from simulation
-        time, tSigBBr = gen_bb_signal(time, tSigTB)
+        ### shift back to baseband (if applied after summing with delays results in impossible peak detection)
+        tSigBBr = tSigTBr * np.exp(-2.j*np.pi*fc*time)
 
-        ### applying SysOrd order butterworth low pass forwards and backwards
-        b, a = sig.butter(sysOrd, bw, 'lowpass', fs=fs, output='ba')
-        tSigBBr = sig.filtfilt(b, a, tSigBBr)
+        ### use recv transfer-band singal for sum
+        lstSignals.append(tSigBBr)
 
-        tSendSig.append(tSigBBr)
+    ### sum up all signals with added delay zero padding and extenden length
+    timeSum, tSigBBrSum = delay_sum(lstSignals, tDelays, fs)
 
-    tSigSum = delay_sum(np.real(tSendSig), tDelays, fs)
-    winLen = fs * 5e-4
+    ### setting win length resulting in a guard bin size of peak width 0.00014
+    guLen = int(fs * 0.00014)
 
-    """figure = make_subplots(rows=numOfAnchors, cols=1)
+    ### add white noise to not delayed part
+    gwn = gen_gwn_snr(tSigBBrSum, targetSNR)
+    tSigBBrSum += gwn
+    print("SNR_dB:", 10 * np.log10(sig_noise_ratio(tSigBBrSum - gwn, gwn)), "dB")
+    print("SNR:", sig_noise_ratio(tSigBBrSum - gwn, gwn))
+
+    ### applying SysOrd order butterworth low pass forwards and backwards
+    b, a = sig.butter(sysOrd, bw, 'lowpass', fs=fs, output='ba')
+    tSigBBrSum = sig.filtfilt(b, a, tSigBBrSum)
+
+    figure = make_subplots(rows=numOfAnchors, cols=1)
     index = 1
-    for si in tSendSig:
+    for si in lstAnchors:
 
-        si = np.append(si, [0] * (len(tSigSum[1]) - len(si)))
-        SigCC = corr_lag(np.real(tSigSum[1]), np.real(si), fs)
-        varSigSum = ca_cfar(SigCC[1], int(winLen), int(0.1 * winLen), 1e-3, sort=False)
+        si = np.append(si, [0] * (len(tSigBBrSum) - len(si)))
+        SigCC = corr_lag(np.real(tSigBBrSum), np.real(si), fs)
+        varSigSum = ca_cfar(SigCC[1], guLen * 10, guLen, 5e-3, sort=False)
         lagInd = np.argmax(SigCC[1])
 
         figure.add_trace(go.Scatter(x=SigCC[0], y=SigCC[1], mode='lines', marker_color='#000'), row=index, col=1)
@@ -342,37 +347,36 @@ def simulation(tDelays: list[float], numOfAnchors: int, addGWN = False, startSee
 
         fBB, fSigBB = _get_fftfunc(tSigBB, fs)
         fTB, fSigTB = _get_fftfunc(tSigTB, fs)
-        fBBr, fSigBBr = _get_fftfunc(tSigBBr, fs)
+        fBBr, fSigBBrSum = _get_fftfunc(tSigBBrSum, fs)
 
         print("signal length in s", len(tSigTB) / fs)
 
 
-        # remove delay and extended time if recieved baseband imported from watermark
-        if usesim:
-            time = time[len(time)//2:]
-            tSigBBr = tSigBBr[int(tDelays[-1] * fs):]
-
         ### code to transfer band process plots and backwards process plots
         figure = make_subplots(rows=3, cols=1)
-        figure.add_trace(go.Bar(y=rawCode, name="bit domain", marker_color='#19D3F3'), row=1, col=1)
-        figure.add_trace(go.Scatter(x=time, y=tSigBB, mode='lines', name="baseband signal", marker_color='#EF553B'), row=2, col=1)
-        figure.add_trace(go.Scatter(x=time, y=tSigTB, mode='lines', name="carrier signal", marker_color='#636EFA'), row=2, col=1)
-        figure.add_trace(go.Scatter(x=time, y=np.real(tSigBBr), mode='lines', name="recv baseband signal", marker_color='#FFA51A'), row=2, col=1)
+        figure.add_trace(go.Bar(y=bCode, name="bit domain", marker_color='#19D3F3'), row=1, col=1)
+        figure.add_trace(go.Scatter(x=time, y=np.real(tSigBB), mode='lines', name="baseband signal", marker_color='#EF553B'), row=2, col=1)
+        figure.add_trace(go.Scatter(x=time, y=np.real(tSigTB), mode='lines', name="carrier signal", marker_color='#636EFA'), row=2, col=1)
         figure.add_trace(go.Scatter(x=fBB, y=np.abs(fSigBB), fill='tozeroy', mode='lines', name="baseband", marker_color='#EF553B'), row=3, col=1)
         figure.add_trace(go.Scatter(x=fTB, y=np.abs(fSigTB), fill='tozeroy', mode='lines', name="carrier", marker_color='#636EFA'), row=3, col=1)
-        figure.add_trace(go.Scatter(x=fBBr, y=np.abs(fSigBBr), fill='tozeroy', mode='lines', name="recv baseband", marker_color='#FFA51A'), row=3, col=1)
-        figure.update_layout(title='Baseband & Transmission-Band Processing')
+        figure.update_layout(title='Baseband & Transmission-Band of Send Signal')
         figure.show()
 
-        # TODOS: 
-        # SNR verdammt nochmal hinbekommen
-        # simulation so feststellen, dass singal ohne delay genommen wird
-        # kombinationen mit SNR festmachen"""
-        
-
+        figure = make_subplots(rows=2, cols=1)
+        figure.add_trace(go.Scatter(x=timeSum, y=np.real(tSigBBrSum), mode='lines', name="recv baseband signal", marker_color='#FFA51A'), row=1, col=1)
+        figure.add_trace(go.Scatter(x=fBBr, y=np.abs(fSigBBrSum), fill='tozeroy', mode='lines', name="recv baseband", marker_color='#FFA51A'), row=2, col=1)
+        figure.update_layout(title='Recieved Sum of Signals in Baseband')
+        figure.show()
 
 def main():
-    simulation([20e-3, 30e-3, 40e-3], 3, showAll=False, addGWN=True)
+    global file_index, load_index
+    lstSNRdB = [-20, -15, -10, -5, 0, 5, 10]
+    for snr in lstSNRdB:
+        snr = 10 ** (snr / 10)
+        simulation([10e-3, 30e-3, 40e-3], 3, showAll=False, addGWN=True, targetSNR=snr)
+        file_index = 0
+        load_index = 0
+
     #showButterBode()
     #showRaisedCosine()
 
